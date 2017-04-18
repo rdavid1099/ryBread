d45273a074bf13e47a48c3e6f01e4d67e3149229
Ryan Workman <rdavid1099@gmail.com>
2017-04-18 11:22:47 -0600

    KeyGenerator supports base64 encryption keys

--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -9,8 +9,15 @@ class KeyGeneratorTest < TestHelper
   end
 
   def test_it_generates_key_with_whatever_amount
-    assert_equal 20, key_generator.new_key(20).length
-    assert_equal 10, key_generator.new_key(10).length
-    assert_equal 50, key_generator.new_key(50).length
+    assert_equal 20, key_generator.new_key(length: 20).length
+    assert_equal 10, key_generator.new_key(length: 10).length
+    assert_equal 50, key_generator.new_key(length: 50).length
+  end
+
+  def test_it_generates_base_64_key
+    key = key_generator.new_key encryption: :base64
+
+    assert_equal 27, key.length
+    assert key.chars.all? { |x| true if x =~ /[A-Za-z0-9]/ }
   end
 end
